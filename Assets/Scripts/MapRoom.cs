6e391e964271471c0b17dda7ce265a958bb5c07a
ChesterLuna <chester99124@gmail.com>
2023-12-03 03:19:51 -0700

    Added any shape button

--- Assets/Scripts/MapRoom.cs
+++ Assets/Scripts/MapRoom.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MapRoom : MonoBehaviour
 {
     //[SerializeField] string sceneToLoad;
 
+    private void Start()
+    {
+        GetComponent<Image>().alphaHitTestMinimumThreshold = 1f;
+    }
 
     public void ChangeScene(string sceneToLoad)
     {
